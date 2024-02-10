8d4d2aae672300150e9c408d409c6d20c5950f04
hype1356 <swayamkan@gmail.com>
2024-02-10 19:43:35 +0000

    context generator

--- src/embeddings_gen.py
+++ src/embeddings_gen.py
@@ -9,7 +9,7 @@ for l in file_input:
   sentences.append(l)
 
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L12-v2')
-embeddings = model.encode(sentences)
+embeddings = model.encode(sentences, show_progress_bar=True)
 
 np.save(filename + '_embeddings', embeddings)
 print("Saved as " + filename + "_embeddings")
