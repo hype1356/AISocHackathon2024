1f79602d141984766065de44367a9e1b7d3c5900
Antantant1 <anthonypig@hotmail.com>
2024-02-11 03:33:08 +0000

    xml parser

--- contextData/xml_parser.py
+++ contextData/xml_parser.py
@@ -1,58 +1,52 @@
-import re
 import lxml.etree as ET
+import re
 
-tree = ET.parse("./")
+tree1 = ET.parse("./contextData/vol1.xml")
+tree2 = ET.parse("./contextData/vol2.xml")
+tree3 = ET.parse("./contextData/vol3.xml")
+tree4 = ET.parse("./contextData/vol4.xml")
+tree5 = ET.parse("./contextData/vol5.xml")
 
-# <span class="SECTION PART-SECTION">
-# data = [] #array of lines to output for embeddings (one element = one line)
-# lines = file.readlines() #file's lines
-# lineNumber = 0
+output = open("./contextData/output", "w")
 
-# # increment lineNumber
-# def nextLine():
-#     global lineNumber
-#     lineNumber += 1
+new_root = ET.Element("combined_data")
 
-# #readSection() to read a whole section
-# def readSection():
-#     indent = 1
-#     while(indent > 0):
-#         line = lines[lineNumber]
-#         closeSpan = re.findall("(</span>)", line) #span by itself
-#         openSpan = re.findall("(<span class=)", line)
-#         indent += len(openSpan)
-#         indent -= len(closeSpan)
-#         nextLine()
-#     return ""
-        
-#     # while(indent > 0):
-#     #     line = lines[lineNumber]
-#     #     closeSpan = re.search("</span>", line) #span by itself
-#     #     openSpan = re.search("<span class=", line)
-#     #     if(closeSpan):
-#     #         # print("CLOSE")
-#     #         if(openSpan):
-#     #             pass
-#     #         else:
-#     #             indent -= 1
-#     #         nextLine()
-#     #         continue
-#     #     if(openSpan):
-#     #         # print("OPEN")
-#     #         indent += 1
-#     #         nextLine()
-#     #         continue
-#     #     nextLine()
-#     # return ""
-        
+for tree in [tree1, tree2, tree3, tree4, tree5]:
+    for child in tree.getroot():
+        new_root.append(child)
 
-# while(lineNumber < len(lines)):
-#     line = lines[lineNumber]
-    
-#     sectionSpan = re.search("<span class=\"SECTION PART-SECTION\">", line)
-#     if(sectionSpan):
-#         nextLine()
-#         data.append(readSection()) #append string return to data array
-#         continue
-#     nextLine()
-    
+tree = ET.ElementTree(new_root)
+
+target_section = tree.xpath("//SECTION")
+def prettyToString(section):
+    str1 = re.sub(r"(^\s+)|(\n)", "", ET.tostring(section, encoding="unicode", method="text"))
+    str = re.sub(r"\s{2,}", " ", str1)
+    return str
+
+#TEMP TO DELETE
+def dirtyToString(section):
+    return ET.tostring(section, encoding="unicode")
+
+def oneOne(section):
+    ps = section.xpath(".//NOTE") + section.xpath(".//P")[1:]
+    ps = list(map(lambda p : prettyToString(p), ps))
+    for i in range(len(ps) - 1, 0, -1):
+        paren = re.search("^\(", ps[i])
+        if(paren):
+            ps[i - 1] += ps[i]
+            ps.pop(i)
+    for p in ps:
+        output.write(p + "\n")
+
+def oneTwo(section):
+    ps = section.xpath(".//P")[1:]
+    for p in ps:
+
+        output.write(prettyToString(p) + "\n")
+
+oneOne(target_section[0])
+oneTwo(target_section[1])
+
+for section in target_section[2:]:
+    str = prettyToString(section)
+    output.write(str + "\n")
